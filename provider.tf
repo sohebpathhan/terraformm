ebbd503d2f5a53db73fa996a273356752ceeff3d
soheb <sohebpathan@sohebs-MacBook-Air.local>
2025-12-20 15:39:29 +0530

    statergy change

--- provider.tf
+++ provider.tf
@@ -5,7 +5,7 @@ provider "aws" {
 
 # allow for all ipv4 traffic
 resource "aws_security_group" "allow_ssh" {
-  name        = "defsg"
+  name        = "defsg2"
   description = "Allow SSH inbound traffic"
   vpc_id = var.vpc_id
 
@@ -21,12 +21,6 @@ resource "aws_security_group" "allow_ssh" {
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
-    }
-    ingress {
-      from_port   = 0
-      to_port     = 65535
-      protocol    = "tcp"
-      cidr_blocks = ["0.0.0.0/0"]
     }
     egress {
       from_port   = 0
@@ -44,12 +38,10 @@ resource "aws_launch_template" "home_template" {
   instance_type = "t3.micro"
   key_name      = "micro_central"
 
-  network_interfaces {
-    security_groups = [aws_security_group.allow_ssh.id]
-    associate_public_ip_address = true
-  }
+ 
+  vpc_security_group_ids = [ aws_security_group.allow_ssh ]
 
-  user_data = <<-EOF
+  user_data = base64encode( <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y httpd
@@ -57,6 +49,7 @@ resource "aws_launch_template" "home_template" {
               systemctl enable httpd
               echo "<h1>Welcome to my home page.shoheb</h1>" > /var/www/html/index.html
               EOF
+              )
 }
 
 # crete teplate for mobile
@@ -66,12 +59,9 @@ resource "aws_launch_template" "mobile_template" {
   instance_type = "t3.micro"
   key_name      = "micro_central"
 
-  network_interfaces {
-    security_groups = [aws_security_group.allow_ssh.id]
-    associate_public_ip_address = true
-  }
+  vpc_security_group_ids = [ aws_security_group.allow_ssh ]
 
-  user_data = <<-EOF
+  user_data = base64encode( <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y httpd
@@ -79,7 +69,8 @@ resource "aws_launch_template" "mobile_template" {
               systemctl enable httpd
               echo "<h1>Welcome to my mobile page.shoheb</h1>" > /var/www/html/index.html
               EOF
-}
+              )
+} 
 
 # crete template for laptop
 resource "aws_launch_template" "laptop_template" {
@@ -88,12 +79,9 @@ resource "aws_launch_template" "laptop_template" {
   instance_type = "t3.micro"
   key_name      = "micro_central"
 
-  network_interfaces {
-    security_groups = [aws_security_group.allow_ssh.id]
-    associate_public_ip_address = true
-  }
+  vpc_security_group_ids = [ aws_security_group.allow_ssh ]
 
-  user_data = <<-EOF
+  user_data = base64encode( <<-EOF
               #!/bin/bash
               yum update -y
               yum install -y httpd
@@ -101,8 +89,8 @@ resource "aws_launch_template" "laptop_template" {
               systemctl enable httpd
               echo "<h1>Welcome to my laptop page.shoheb</h1>" > /var/www/html/index.html
               EOF
+              )
 }
-
 # create auto scaling group for home
 resource "aws_autoscaling_group" "home_asg" {
   desired_capacity     = 1
@@ -112,7 +100,10 @@ resource "aws_autoscaling_group" "home_asg" {
     id      = aws_launch_template.home_template.id
     version = "$Latest"
   }
-    vpc_zone_identifier  = [var.subnet_id-1, var.subnet_id-2]
+    vpc_zone_identifier  = [
+      var.subnet_id-1,
+       var.subnet_id-2
+       ]
     tag {
         key                 = "Name"
         value               = "home-asg-instance"
@@ -125,19 +116,18 @@ resource "aws_autoscaling_policy" "home_scale_up" {
   name                   = "home-scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.home_asg.name
   policy_type = "PredictiveScaling"   
-  predictive_scaling_configuration {
-    metric_specification {
-      target_value = 50
-      customized_load_metric_specification {
-        metric_data_queries {
-          id         = "load_sum"
-          expression = "SUM(SEARCH('{AWS/EC2,AutoScalingGroupName} MetricName=\"CPUUtilization\" my-test-asg', 'Sum', 3600))"
-        }       
+    predictive_scaling_configuration {
+  metric_specification {
+    target_value = 50
 
-      }
+    predefined_load_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
     }
   }
 }
+
+    }
+
 
 
 
@@ -150,7 +140,10 @@ resource "aws_autoscaling_group" "mobile_asg" {
     id      = aws_launch_template.mobile_template.id
     version = "$Latest"
   }
-    vpc_zone_identifier  = [var.subnet_id-1, var.subnet_id-2]
+    vpc_zone_identifier  = [
+      var.subnet_id-1, 
+      var.subnet_id-2
+      ]
     tag {
         key                 = "Name"
         value               = "mobile-asg-instance"
@@ -163,19 +156,19 @@ resource "aws_autoscaling_policy" "mobile_scale_up" {
   name                   = "mobile-scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.mobile_asg.name
   policy_type = "PredictiveScaling"   
-  predictive_scaling_configuration {
-    metric_specification {
-      target_value = 50
-      customized_load_metric_specification {
-        metric_data_queries {
-          id         = "load_sum"
-          expression = "SUM(SEARCH('{AWS/EC2,AutoScalingGroupName} MetricName=\"CPUUtilization\" my-test-asg', 'Sum', 3600))"
-        }       
+   predictive_scaling_configuration {
+  metric_specification {
+    target_value = 50
 
-      }
+    predefined_load_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
     }
   }
 }
+ 
+   }    
+
+    
 
 # create auto scaling group for laptop
 resource "aws_autoscaling_group" "laptop_asg" {
@@ -186,7 +179,10 @@ resource "aws_autoscaling_group" "laptop_asg" {
     id      = aws_launch_template.laptop_template.id
     version = "$Latest"
   }
-    vpc_zone_identifier  = [var.subnet_id-1, var.subnet_id-2]
+    vpc_zone_identifier  = [
+      var.subnet_id-1, 
+      var.subnet_id-2
+      ]
     tag {
         key                 = "Name"
         value               = "laptop-asg-instance"
@@ -199,16 +195,14 @@ resource "aws_autoscaling_policy" "laptop_scale_up" {
   name                   = "laptop-scale-up-policy"
   autoscaling_group_name = aws_autoscaling_group.laptop_asg.name
   policy_type = "PredictiveScaling"
-    predictive_scaling_configuration {
-        metric_specification {
-        target_value = 50
-        customized_load_metric_specification {
-            metric_data_queries {
-            id         = "load_sum"
-            expression = "SUM(SEARCH('{AWS/EC2,AutoScalingGroupName} MetricName=\"CPUUtilization\" my-test-asg', 'Sum', 3600))"
-            }       
-    
-        }
-        }
+   predictive_scaling_configuration {
+  metric_specification {
+    target_value = 50
+
+    predefined_load_metric_specification {
+      predefined_metric_type = "ASGAverageCPUUtilization"
     }
+  }
 }
+
+        }
