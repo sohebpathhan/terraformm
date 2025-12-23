7dd24964c5ba865b84911e33fe719a8c235eeeef
soheb <sohebpathan@sohebs-MacBook-Air.local>
2025-12-24 00:31:43 +0530

    vpc

--- create vpc/main.tf
+++ create vpc/main.tf
@@ -11,7 +11,14 @@ resource "aws_vpc" "main" {
 resource "aws_subnet" "public_subnet" {
   vpc_id                  = aws_vpc.main.id
   cidr_block              = "10.0.1.0/24"       
-    ma
+ map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public_subnet"
+  }
+}
+
+
 # igw
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
